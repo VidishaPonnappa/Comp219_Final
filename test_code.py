e12bea7c59ef3a47ebd1448dbad6ae43185a6ff6
ArcticMonkey101 <vidisha_ponnappa.sias21@krea.ac.in>
2023-09-20 21:08:57 +0530

    Three files added

--- test_code.py
+++ test_code.py
@@ -1,4 +1,11 @@
-with open('sample_data.txt', 'r') as file:
-    data = file.read()
+import pandas as pd
 
-print(data)
+# Read data from job_data.csv
+data = pd.read_csv('job_data.csv')
+
+# Compute the average of num_of_salaries for specific job titles
+job_titles = ["Data Architect", "Senior Business Analyst", "Data Scientist", "Machine Learning Engineer", "Senior Data Engineer"]
+
+for title in job_titles:
+    avg_salary = data[data['job_title'] == title]['num_of_salaries'].mean()
+    print(f"Average num_of_salaries for {title}: {avg_salary}")
